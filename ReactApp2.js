562eb36b7301362eaecb5d8465062c9c9ce8b3ac
Mona Abdollahi <abd.mona@hotmail.com>
2017-04-21 17:45:54 +0200

    add in list

--- ReactApp2.js
+++ ReactApp2.js
@@ -1,40 +1,12 @@
 
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-
 class App extends React.Component {
-  constructor(props){
+  constructor(props){  // when we have state we must have constructor****
     super(props);
     this.state={
       name:'',
-       age: ''
-    }
-    this.setNameAndAge=this.setNameAndAge.bind(this);
-  }
-  render(){
-    return(
-      <div>
-      <MyInput nameAndAge={this.setNameAndAge}/> 
-      /*nameAndAge is a props name which //we choosed ourself and to creat prop we just give our props a name and //put the function in it and in other component we can say with help of //'props' that we want to use this func
-          */      
-        <MyList nameAndAge={this.setNameAndAge}/> </div>
-      
-    )//return
-  }//render
-  setNameAndAge(nameA,ageA){
-    this.setState({
-      name: nameA,
-      age: ageA
-    });
-  }
-}//Appcomp
-   
-   class MyList extends React.Component {
-    constructor(props) {
-			super(props);
-			this.state = {
-			  myList : [
+      age: '',
+      oldList: [
            {
             name: "Mark",
             age: "30"
@@ -47,84 +19,81 @@ class App extends React.Component {
             name: "Tony",
             age: "40"
             }
-        ],
-        newObject: {
-        
-         name: 'Mona',
-         age: 20
-        }
-			};//this state
-			 this.handleChange=this.handleChange.bind(this);
-    }//const
-    
-    
-     render(){
+      ]
+    };
+    this.setNameAndAge=this.setNameAndAge.bind(this);
+    this.addNewObject=this.addNewObject.bind(this);
+  }
+  
+  render(){
+    return (<div>
+      <AddForm nameAndAge={this.setNameAndAge} buttonClick={this.addNewObject}/> 
+      <MyList theList={this.state.oldList}/>
+    </div>);
+  }//render
+  
+  setNameAndAge(event){
+    var el = event.target; // input elementet
+    if (el.placeholder == "name")
+      this.setState({ name: el.value });
+    else if (el.placeholder == "age")
+      this.setState({ age: el.value });
+  }
+  
+  addNewObject(event) {
+     var newList = this.state.oldList;
+
+      newList.push({
+        name: this.state.name,
+        age: this.state.age
+      });
+
+      this.setState ({
+          oldList: newList
+      }); //addnewobj
+  }
+}//Appcomp
+   
+   class MyList extends React.Component {
+     constructor(props) {
+       super(props);
+     }
+     
+     render() {
+      const items = this.props.theList.map( item => {
+        return <li>{item.name}, {item.age}</li>;
+      });
+      return <ol>
+          {items}
+       </ol>;
        
-     return  <div><ol>
-                <li>{this.state.myList[0].name + " "+this.state.myList[0].age  }</li>
-                <li>{this.state.myList[1].name + " "+this.state.myList[1].age } </li>
-                <li>{this.state.myList[2].name + " "+this.state.myList[2].age } </li>
-                <li>{this.handleChange}</li> 
-              </ol></div>;
-     } //render
+       
+      return <ol>
+        {this.props.theList.map( item => 
+          <li>{item.name}, {item.age}</li>) }
+      </ol>;
       
-      handleChange(){
-          this.props.nameAndAge(this.state.myList.name, this.state.myList.age);
-      }
+
+     } //render
    }////MyListcomp 
    
    
-     class MyInput extends React.Component {
+     class AddForm extends React.Component {
         constructor(props) {
         super(props);
-        this.handleChangeName=this.handleChangeName.bind(this);
-        this.handleChangeAge= this.handleChangeAge.bind(this);
-        this.handleClick=this.handleClick.bind(this);
+        
         }
        render(){
-         this.props.nameAndAge
+         
          return <div>
-         <input type="text" placeholder="name" onChange={this.handleChangeName}/>
-         <input type="text" placeholder="age" onChange={this.handleChangeAge}/>
-         <button onClick={this.handleClick}>click Add</button></div>;
+         <input type="text" placeholder="name" onChange={this.props.nameAndAge}/>
+         <input type="text" placeholder="age" onChange={this.props.nameAndAge}/>
+         <button onClick={this.props.buttonClick}>click Add</button></div>;
        }//render
         
-       handleClick(){
-          this.props.nameAndAge(this.state.inputname, this.state.inputage);
            //anropa functionen här bara, 
           //when you want to get hold of input someth which is in event you use state ** u see in code up
-       } //click   
-          handleChangeName(event){
-            this.setState({
-               inputname: event.target.value
-          })
-          };//handleChangeName
-          handleChangeAge(event){
-            this.setState({
-               inputage: event.target.value
-          })
-          };//handleChangeage
-        
-       /* function createNewObject(e){
-        
-         let newPerson {
-             name: '',
-             age:''
-        }
-        
-        if(e.target.id=='name'){
-          newPerson.name = e.target.value;
-        };
-        
-        else if(e.target.id=='age'){
-          
-          newPerson.handleChangeName = e.target.value;
-        };
-        
-        this.setState({newObject: newPerson})
-        }
-        };//newObject
-        }  */
+       //click   
        
        }//inputcomp
        
